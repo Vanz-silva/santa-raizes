79c6fdbdfd0bdb4551ad9865c49f4b52c04fbffa
Vanz-silva <162450963+mmlabb@users.noreply.github.com>
2025-05-20 09:30:30 -0300

    fix: ajuste de text about

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -6,18 +6,24 @@ import { FaPaw, FaEye, FaHeart } from "react-icons/fa";
 
 const About = () => (
   <section className="py-5">
-    <div className="container about  py-5 mt-5" style={{ width: "70%" }}>
+    <div
+      className="container about  py-5 mt-5 pt-5 mb-5"
+      style={{ width: "70%" }}
+    >
       <div className="row linha-sobre align-items-center">
         {/* Coluna de texto */}
-        <div className="col-md-6 col-sm-12 text-center">
+        <div className="col-md-8 col-sm-12 text-center">
           <h1>Sobre o projeto</h1>
 
           <h5>
-            Santa Raízes é um projeto que une natureza e tecnologia para
-            promover educação ambiental de forma interativa. Utilizando QR Codes
-            fixados em árvores do Parque da Cidade, os visitantes acessam
-            páginas digitais com informações sobre cada espécie — origem,
-            benefícios e curiosidades.
+            O Santa Raízes é um projeto que une natureza e tecnologia para
+            transformar sua caminhada no parque da Santa Maria -DF em uma jornada de aprendizado
+            ambiental. Por meio de QR Codes instalados em árvores do Parque da
+            Cidade, você pode acessar páginas digitais exclusivas com
+            informações sobre cada espécie — sua origem, benefícios para o meio
+            ambiente e curiosidades surpreendentes. Faça uma visita ao parque,
+            escaneie os códigos com seu celular e descubra como cada árvore
+            conta uma história.
           </h5>
         </div>
       </div>
