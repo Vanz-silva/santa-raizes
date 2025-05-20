1eadd00ac6c33d21e1b47ea49d22017d1cea2d07
Vanz-silva <162450963+mmlabb@users.noreply.github.com>
2025-05-20 17:43:28 -0300

    feat: ajuste grid arvores

--- src/app/components/GridArvores.js
+++ src/app/components/GridArvores.js
@@ -1,24 +1,41 @@
-// components/Services.js
-import React from "react";
+import Link from "next/link";
 import Image from "next/image";
+import { trees } from "@/app/data/trees";
 
-const GridArvores = () => (
-  <section className="services">
-    <h2 className="py-5">Nossas Árvores</h2>
-    <div className="cardsGrid pt-2 pb-2 p-0">
-      {[...Array(20)].map((_, index) => (
-        <div className="card" key={index}>
-          <Image
-            src={`/service-icon-${index + 1}.png`}
-            alt={`Serviço ${index + 1}`}
-            width={64}
-            height={64}
-          />
-          <p>Especialidade {index + 1}</p>
+export default function GridArvores() {
+  return (
+    <section className="py-5">
+      <div className="container">
+        <h2 className="text-center text-success mb-4">Nossas Árvores</h2>
+        <div className="row g-4">
+          {trees.map((tree) => (
+            <div className="col-6  col-md-3" key={tree.slug}>
+              <Link
+                href={`/arvores/${tree.slug}`}
+                className="text-decoration-none"
+              >
+                <div className="card box-tree h-100 text-center">
+                  <Image
+                    src={tree.imagem}
+                    alt={`Árvore ${tree.nome_popular}`}
+                    width={120}
+                    height={120}
+                    className="card-img-top rounded-circle object-fit-cover mx-auto mt-3"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title text-success">
+                      {tree.nome_popular}
+                    </h5>
+                    <p className="card-text text-muted small fst-italic">
+                      {tree.nome_cientifico}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-  </section>
-);
-
-export default GridArvores;
+      </div>
+    </section>
+  );
+}
