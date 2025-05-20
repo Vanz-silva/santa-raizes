d06b3eeff6db7ca4a3b0df358eb9b3b4dbceb12b
Vanz-silva <162450963+mmlabb@users.noreply.github.com>
2025-05-20 17:50:01 -0300

    feat: moc arvores

---
+++ src/app/data/trees.js
@@ -0,0 +1,50 @@
+//(mock de 20 árvores)
+export const trees = [
+  {
+    slug: "ipe-amarelo",
+    nome_popular: "Ipê Amarelo",
+    nome_cientifico: "Handroanthus albus",
+    idade: "15 anos",
+    beneficios: "Atrai polinizadores, melhora a estrutura do solo.",
+    curiosidades: "É símbolo nacional e floresce intensamente no inverno.",
+    imagem: "/arvores/img-pa.jpg",
+  },
+  {
+    slug: "pau-brasil",
+    nome_popular: "Pau Brasil",
+    nome_cientifico: "Paubrasilia echinata",
+    idade: "30 anos",
+    beneficios: "Fixação de nitrogênio, regeneração de solos.",
+    curiosidades: "Deu origem ao nome do nosso país.",
+    imagem: "/arvores/pau-brasil.jpg",
+  },
+  {
+    slug: "brasil",
+    nome_popular: "Pau Brasil",
+    nome_cientifico: "Paubrasilia echinata",
+    idade: "30 anos",
+    beneficios: "Fixação de nitrogênio, regeneração de solos.",
+    curiosidades: "Deu origem ao nome do nosso país.",
+    imagem: "/arvores/pau-brasil.jpg",
+  },
+
+  {
+    slug: "asil",
+    nome_popular: "Pau Brasil",
+    nome_cientifico: "Paubrasilia echinata",
+    idade: "30 anos",
+    beneficios: "Fixação de nitrogênio, regeneração de solos.",
+    curiosidades: "Deu origem ao nome do nosso país.",
+    imagem: "/arvores/pau-brasil.jpg",
+  },
+
+  {
+    slug: "pau",
+    nome_popular: "Pau Brasil",
+    nome_cientifico: "Paubrasilia echinata",
+    idade: "30 anos",
+    beneficios: "Fixação de nitrogênio, regeneração de solos.",
+    curiosidades: "Deu origem ao nome do nosso país.",
+    imagem: "/arvores/pau-brasil.jpg",
+  },
+];
