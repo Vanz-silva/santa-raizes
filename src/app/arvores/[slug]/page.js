10ddf8ddbe4ed575c628d4167ddb672bf6b57a7e
Vanz-silva <162450963+mmlabb@users.noreply.github.com>
2025-05-23 17:10:37 -0300

    feat: Ajuste box-tree

--- src/app/arvores/[slug]/page.js
+++ src/app/arvores/[slug]/page.js
@@ -1,6 +1,8 @@
 import { trees } from "@/app/data/trees";
 import Image from "next/image";
 import { notFound } from "next/navigation";
+import Link from "next/link";
+import { FaLeaf } from "react-icons/fa";
 
 export default function ArvorePage({ params }) {
   const arvore = trees.find((tree) => tree.slug === params.slug);
@@ -8,23 +10,26 @@ export default function ArvorePage({ params }) {
   if (!arvore) return notFound();
 
   return (
-    <div className="container box-tree d-flex justify-content-center py-5 px-3">
-      <div className="row ">
-        <div className="col-10 ">
+    <div className="container  d-flex justify-content-center  py-5 px-3">
+      <div className="row justify-content-center flex-column">
+        <div className="col box-tree ">
           <h1 className="h2 text-success mb-3">{arvore.nome_popular}</h1>
           <p className="mb-4">{arvore.nome_cientifico}</p>
 
-          <div className="mb-4">
+          <div
+            className="mb-4 position-relative"
+            style={{ width: "100%", height: "600px" }}
+          >
             <Image
               src={arvore.imagem}
               alt={`Imagem da árvore ${arvore.nome_popular}`}
-              width={800}
-              height={450}
-              className="img-fluid rounded"
+              fill
+              className="img-fluid rounded object-fit-cover"
+              style={{ borderRadius: "12px" }}
             />
           </div>
 
-          <ul className="list-group list-group-flush">
+          <ul className="list-group p-5 list-group-flush">
             <li className="">
               <h5>
                 <strong>Idade:</strong> {arvore.idade}
@@ -41,6 +46,12 @@ export default function ArvorePage({ params }) {
               </h5>
             </li>
           </ul>
+        </div>
+        <div className="text-center col mb-4 pt-5 pb-5">
+          <Link href="/" className="btn w-100 btn-outline-success">
+            <i className="bi bi-flower1 me-2"></i>Todas
+            Árvores <FaLeaf size={18} className="mb-2" /> 
+          </Link>
         </div>
       </div>
     </div>
