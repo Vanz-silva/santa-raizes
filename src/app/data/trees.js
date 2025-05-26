b2ac6a7c7828e696520b5a846fa9bfd4e42baba7
Vanz-silva <162450963+mmlabb@users.noreply.github.com>
2025-05-25 21:55:13 -0300

    feat: Adicionando mais imagens arvores e atualizando o slug

--- src/app/data/trees.js
+++ src/app/data/trees.js
@@ -1,94 +1,105 @@
-//(mock de 20 árvores)
 export const trees = [
   {
     id: 1,
-    slug: "ipe-amarelo",
-    nome_popular: "Ipê Amarelo",
-    nome_cientifico: "Handroanthus albus",
-    idade: "15 anos",
-    beneficios: "Atrai polinizadores, melhora a estrutura do solo.",
-    curiosidades: "É símbolo nacional e floresce intensamente no inverno.",
-    imagem: "/santa-raizes/arvores/test.jpg",
+    slug: "acacia",
+    nome_popular: "Acácia Bastarda",
+    nome_cientifico: "Robinia  Pseudocacia",
+    beneficios:
+      "Atrai polinizadores, melhora a estrutura do solo, Acácia-Bastarda também possui propriedades medicinais, sendo utilizada no tratamento de problemas respiratórios e de pele, As flores da Acácia-Bastarda  surgem na primavera e são formadas por cachos pendentes de cor branca ou rosa claro, que exalam um perfume doce e suave",
+    curiosidades:
+      "Essa árvore incrível é originária da América do Norte e foi introduzida no Brasil na década de 1920. Com sua beleza exótica e imponente.",
+    imagem: "/santa-raizes/arvores/acacia.jpeg",
   },
   {
     id: 2,
-    slug: "pau-brasil",
-    nome_popular: "Pau Brasil",
-    nome_cientifico: "Paubrasilia echinata",
-    idade: "30 anos",
-    beneficios: "Fixação de nitrogênio, regeneração de solos.",
-    curiosidades: "Deu origem ao nome do nosso país.",
-    imagem: "/santa-raizes/arvores/pau-brasil.jpg",
+    slug: "tipuana",
+    nome_popular: "Tipuana Tipu",
+    nome_cientifico: "Benth Kuntze",
+    beneficios:
+      "A Tipuana Tipu (Tipuana tipu) oferece diversos benefícios para o solo, como a melhora da qualidade do solo e a prevenção de erosão. A sua copa densa protege o solo da chuva forte, e as folhas que caem fornecem matéria orgânica, enriquecendo o solo e melhorando a sua estrutura.",
+    curiosidades:
+      "Conhecida como tipuana ou amendoim-acácia, é originária da América do Sul no Norte da Argentina e na Bolívia. Os climas preferidos dessa planta são o temperado, tropical e o subtropical, tornando-se capaz de suportar longas estiagens. A Tipuana é bem comum no Sul da Califórnia e muitas das árvores da região foram invadidas por insetos chamados psilídeo Tipu, dando origem do nome da Tipuana.",
+    imagem: "/santa-raizes/arvores/tipuana1.jpeg",
   },
   {
     id: 3,
-    slug: "brasil",
-    nome_popular: "Pau Brasil",
-    nome_cientifico: "Paubrasilia echinata",
-    idade: "30 anos",
-    beneficios: "Fixação de nitrogênio, regeneração de solos.",
-    curiosidades: "Deu origem ao nome do nosso país.",
-    imagem: "/santa-raizes/arvores/pau-brasil.jpg",
+    slug: "barbatimao",
+    nome_popular: "Barbatimão",
+    nome_cientifico: "Stryphnodendron adstringens",
+    beneficios:
+      "O barbatimão, conhecido cientificamente como Stryphnodendron adstringens, é uma planta nativa do cerrado brasileiro amplamente utilizada na medicina tradicional",
+    curiosidades:
+      "Seu nome, originado do tupi yba-timão, significa árvore que aperta, em referência às suas propriedades adstringentes. A casca da planta concentra compostos bioativos que oferecem uma série de benefícios terapêuticos. Não à toa, no Brasil, ela está listada na Relação Nacional de Plantas Medicinais de Interesse ao SUS (RENISUS) e no Formulário de Fitoterápicos da Farmacopeia Brasileira (2ª edição) da Agência Nacional de Vigilância Sanitária (Anvisa).",
+    imagem: "/santa-raizes/arvores/barbati.jpeg",
   },
   {
     id: 4,
-    slug: "asil",
-    nome_popular: "Pau Brasil",
-    nome_cientifico: "Paubrasilia echinata",
-    idade: "30 anos",
-    beneficios: "Fixação de nitrogênio, regeneração de solos.",
-    curiosidades: "Deu origem ao nome do nosso país.",
-    imagem: "/santa-raizes/arvores/pau-brasil.jpg",
+    slug: "libidibia",
+    nome_popular: "Pau Ferro",
+    nome_cientifico: "Libidibia Ferrea",
+    beneficios:
+      "O pau-ferro, Caesalpinia ferrea, é uma árvore abundante nas matas dos estados que vão desde São Paulo até o Piauí. Madeira dura, de ferro, como o nome bem diz, também é conhecida como jucá, jucaina e diversos outros nomes de origem indígena. Mais do que duro e forte, o pau-ferro é uma planta medicinal muito usada contra bronquites, reumatismo e sífilis e agora, ficou demonstrado seu efeito na cura de úlceras gástricas.",
+    curiosidades:
+      "A madeira da pau-ferro é muito pesada, dura e longeva. Por isso, usa-se a madeira da pau-ferro para a confecção de vigas, esteios, estacas e lenha. Infelizmente, por essa razão, ela tem sido devastada e encontra-se atualmente como uma das árvores ameaçadas de extinção.",
+    imagem: "/santa-raizes/arvores/pau-ferro.jpeg",
   },
   {
     id: 5,
-    slug: "pau",
-    nome_popular: "Pau Brasil",
-    nome_cientifico: "Paubrasilia echinata",
-    idade: "30 anos",
-    beneficios: "Fixação de nitrogênio, regeneração de solos.",
-    curiosidades: "Deu origem ao nome do nosso país.",
-    imagem: "/santa-raizes/arvores/pau-brasil.jpg",
+    slug: "cega-machado",
+    nome_popular: "Cega Machado",
+    nome_cientifico: "Physocalymma Scaberrimum",
+
+    beneficios:
+      "A cega machado, também conhecida como cega de machado ou cega de machado de campo, é uma planta forrageira muito utilizada na pecuária brasileira, especialmente em sistemas de pastagem. Essa planta é valorizada por sua resistência e capacidade de adaptação a diferentes tipos de solo e clima, além de proporcionar uma boa qualidade nutricional para os animais. O cultivo da cega machado é uma prática comum entre os produtores que buscam aumentar a produtividade de suas propriedades rurais.",
+    curiosidades:
+      "A cega machado oferece diversos benefícios para a pecuária, como a melhoria da qualidade do pasto e o aumento da produtividade animal. Sua alta palatabilidade e valor nutricional contribuem para um melhor ganho de peso dos animais, além de promover a saúde do rebanho. Além disso, o cultivo da cega machado pode ser uma alternativa sustentável, pois ajuda a conservar o solo e a biodiversidade, integrando práticas de agroecologia e sustentabilidade no agronegócio.",
+    imagem: "/santa-raizes/arvores/cega-machado.jpeg",
   },
 
   {
     id: 6,
-    slug: "ipe-amarelo",
-    nome_popular: "Ipê Amarelo",
-    nome_cientifico: "Handroanthus albus",
-    idade: "15 anos",
-    beneficios: "Atrai polinizadores, melhora a estrutura do solo.",
-    curiosidades: "É símbolo nacional e floresce intensamente no inverno.",
-    imagem: "/santa-raizes/arvores/test.jpg",
+    slug: "caviuna",
+    nome_popular: "Caviúna",
+    nome_cientifico: "Machaerium scleroxylon",
+    beneficios:
+      "A espécie Caviúna é perenifólia, com 10 a 20 m de altura e 30 a 50 cm de diâmetro. O tronco é levemente tortuoso e a casca externa possui coloração cinza-escura a marrom-escura, lisa, lenticelada e esfoliante.",
+    curiosidades:
+      "A madeira de caviúna é indicada para construção civil, móveis finos, painéis, marchetaria, peças torneadas, molduras, objeto de adorno, tacos para bilhar e cabos de ferramentas. Além disso, a espécie é recomendada para reposição de mata ciliar, em locais sem inundação.",
+    imagem: "/santa-raizes/arvores/caviuna.jpeg",
   },
   {
     id: 7,
-    slug: "pau-brasil",
-    nome_popular: "Pau Brasil",
-    nome_cientifico: "Paubrasilia echinata",
-    idade: "30 anos",
-    beneficios: "Fixação de nitrogênio, regeneração de solos.",
-    curiosidades: "Deu origem ao nome do nosso país.",
-    imagem: "/santa-raizes/arvores/pau-brasil.jpg",
+    slug: "robinia-viscosa",
+    nome_popular: "Robinia Viscosa",
+    nome_cientifico: "Robinia Viscosa",
+    beneficios:
+      "A Robinia viscosa, conhecida popularmente como robinia pegajosa, glicínia-falsa, ou simplesmente falsa-acácia-rosa, é uma árvore da família Fabaceae, nativa do sudeste dos Estados Unidos. Ela é apreciada por sua beleza ornamental e suas flores vistosas de cor rosa-avermelhada na primavera.",
+    curiosidades:
+      "Amiga das abelhas: Suas flores são altamente melíferas. Abelhas produzem um mel escuro e perfumado a partir dela.Fixadora de nitrogênio: Como outras leguminosas, ajuda a enriquecer o solo, melhorando a fertilidade naturalmente. Não é a mesma que a Robinia pseudoacacia: Apesar da semelhança no nome, esta última (acácia-negra ou falsa-acácia) é mais usada em reflorestamento e possui flores brancas.",
+    imagem: "/santa-raizes/arvores/robinia2.jpeg",
   },
   {
     id: 8,
-    slug: "brasil",
-    nome_popular: "Pau Brasil",
-    nome_cientifico: "Paubrasilia echinata",
-    idade: "30 anos",
-    beneficios: "Fixação de nitrogênio, regeneração de solos.",
-    curiosidades: "Deu origem ao nome do nosso país.",
-    imagem: "/santa-raizes/arvores/pau-brasil.jpg",
+    slug: "ameixinha",
+    nome_popular: "Ameixinha Amarela",
+    nome_cientifico: "Rhaphiolepis bibas",
+    beneficios:
+      "A árvore, ao crescer, ajuda a melhorar a estrutura do solo, a fixar nutrientes e a aumentar a capacidade de retenção de água. Além disso, a adubação natural que ocorre com a queda das folhas e frutos contribui para a fertilidade do solo. ",
+    curiosidades:
+      "Originária da China, muito cultivada no restante do mundo. Tornou-se uma das árvores frutíferas exóticas mais comuns em todo o território nacional. .",
+    imagem: "/santa-raizes/arvores/ameixinha.jpeg",
   },
   {
     id: 9,
-    slug: "asil",
-    nome_popular: "Pau Brasil",
-    nome_cientifico: "Paubrasilia echinata",
-    idade: "30 anos",
-    beneficios: "Fixação de nitrogênio, regeneração de solos.",
-    curiosidades: "Deu origem ao nome do nosso país.",
-    imagem: "/santa-raizes/arvores/pau-brasil.jpg",
+    slug: "cordia",
+    nome_popular: "Cordia Gerascanthus",
+    nome_cientifico: "Cordia Gerascanthus",
+    beneficios:
+      "Um dos benefícios mais notáveis de Cordia subcordata é sua capacidade de estabilizar o solo. O robusto sistema radicular de Cordia subcordata ajuda a ancorar o solo, especialmente em áreas costeiras propensas à erosão. Isso o torna um componente crucial dos ecossistemas costeiros, onde sua presença pode prevenir a degradação e a perda do solo.",
+    curiosidades:
+      "Cordia subcordata contribui para a biodiversidade ao fornecer habitats para diversas espécies. As flores atraem polinizadores como abelhas e borboletas, enquanto pássaros e outros pequenos animais encontram abrigo entre seus galhos. Isso cria um mini-ecossistema que suporta diferentes formas de vida, promovendo a biodiversidade geral.  Cordia subcordata têm sido utilizadas em remédios populares. Suas folhas, casca e flores têm sido empregadas em práticas transmitidas através das gerações, contribuindo para a riqueza cultural da planta.",
+    imagem: "/santa-raizes/arvores/cordia.jpeg",
   },
+
+
 ];
